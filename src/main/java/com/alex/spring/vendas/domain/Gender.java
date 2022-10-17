045029cf06dc340609f5ae3195b45a9ba87caf66
Alexsander <alexsander_lima99@hotmail.com>
2022-10-17 15:08:32 -0300

    Update ClientPost

--- src/main/java/com/alex/spring/vendas/domain/Gender.java
+++ src/main/java/com/alex/spring/vendas/domain/Gender.java
@@ -1,5 +1,7 @@
 package com.alex.spring.vendas.domain;
 
+import com.alex.spring.vendas.exceptions.ArgumentNotValidException;
+
 public enum Gender {
     M("Masculino"), F("Feminino");
 
@@ -8,6 +10,21 @@ public enum Gender {
     }
 
     private final String name;
+
+    public static Gender newGender(String value) {
+        switch (value.toUpperCase()) {
+            case "M" -> {
+                return Gender.M;
+            }
+            case "F" -> {
+                return Gender.F;
+            }
+            default -> {
+                throw new ArgumentNotValidException("The `%s` value is not one of the values accepted [M, F]."
+                        .formatted(value));
+            }
+        }
+    }
 
     public String getName() {
         return this.name;
--- src/main/java/com/alex/spring/vendas/requests/client/ClientPost.java
+++ src/main/java/com/alex/spring/vendas/requests/client/ClientPost.java
@@ -1,4 +1,4 @@
-package com.alex.spring.vendas.requests;
+package com.alex.spring.vendas.requests.client;
 
 import com.alex.spring.vendas.domain.Address;
 import com.alex.spring.vendas.domain.Client;
@@ -16,16 +16,17 @@ public class ClientPost {
     @NotBlank(message = "The field `name` cannot be empty.")
     @Length(min = 3, message = "The field `name` should've minimum three characters.")
     private String name;
-    @NotNull(message = "The field `gender` should've only one character.")
-    private Gender gender;
+    @NotBlank(message = "The field `gender` should've one character.")
+    @Length(min = 1, max = 1, message = "The field `gender` should've only one character -> [M, F].")
+    private String gender;
     @NotNull(message = "The field `address` cannot be null.")
-    @Size(min = 1, message = "The field `address` should've minimum one address")
+    @Size(min = 1, message = "The field `address` should've minimum one address.")
     private List<Address> address;
 
     public Client newClient() {
         Client newClient = new Client();
         newClient.setName(this.name);
-        newClient.setGender(this.gender);
+        newClient.setGender(Gender.newGender(gender));
         newClient.setAddress(this.address);
         newClient.setCreated_at(LocalDate.now());
         return newClient;
@@ -35,7 +36,7 @@ public class ClientPost {
         return name;
     }
 
-    public Gender getGender() {
+    public String getGender() {
         return gender;
     }
 
