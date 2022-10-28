7e87cc946b02efcb5c5fa97f77928769d4620690
Alexsander <alexsander_lima99@hotmail.com>
2022-10-28 11:37:07 -0300

    Add Product

---
+++ src/main/java/com/alex/spring/vendas/domain/Product.java
@@ -0,0 +1,85 @@
+package com.alex.spring.vendas.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private BigDecimal price;
+    private Category category;
+    private String description;
+    @Column(nullable = false)
+    private Long amount = 0L;
+    private String imageName;
+    @Column(nullable = false)
+    private LocalDate created_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+}
---
+++ src/main/java/com/alex/spring/vendas/requests/product/ProductPost.java
@@ -0,0 +1,53 @@
+package com.alex.spring.vendas.requests.product;
+
+import com.alex.spring.vendas.domain.Category;
+import com.alex.spring.vendas.domain.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ProductPost {
+
+    @NotBlank(message = "The field `name` cannot be empty.")
+    @Length(min = 3, message = "The field `name` should've minimum three characters.")
+    private String name;
+    @NotNull(message = "The field `price` cannot be empty.")
+    private BigDecimal price;
+    private Category category;
+    private String description;
+    private Long amount;
+
+    public Product newProduct() {
+        Product newProduct = new Product();
+        newProduct.setName(this.name);
+        newProduct.setPrice(this.price);
+        newProduct.setCategory(this.category);
+        newProduct.setDescription(this.description);
+        newProduct.setAmount(this.amount);
+        newProduct.setCreated_at(LocalDate.now());
+        return newProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+}
